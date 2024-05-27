04c75213803ecc46841bb61df3b5078362e0b39f
Marcelo Oliveira <marcelo@exemplo.com>
2024-05-27 14:38:10 -0300

    viewsets

--- product/serializers/product_serializer.py
+++ product/serializers/product_serializer.py
@@ -5,13 +5,25 @@ from product.serializers.category_serializer import CategorySerializer
 
 class ProductSerializer(serializers.ModelSerializer):
     category = CategorySerializer(required=True, many=True)
+    categories_id = serializers.PrimaryKeyRelatedField(queryset=category.objects.all(), write_only=True, many=True)
 
     class Meta:
         model = Product
         fields = [
+            'id',
             'title',
             'description',
             'price',
             'active',
             'category',
+            'categories_id',
         ]
+
+    def create(self, validated_data):
+        category_data = validated_data.pop('categories_id')
+
+        product = Product.objects.create(**validated_data)
+        for category in category_data:
+            product.category.add(category)
+
+        return product
